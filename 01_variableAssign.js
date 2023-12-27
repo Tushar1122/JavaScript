71fc5a7e53c5bf10c54d61f6a838ed45446970a8
Tushar1122 <Tgi1420@gmail.com>
2023-12-27 22:12:30 +0530

    assignment 1
    assignment 1

---
+++ 01_variableAssign.js
@@ -0,0 +1,30 @@
+var myFullName; ;
+myFullName = "Tushar Imade";
+console.log("Name ->",myFullName);
+
+myFullName = "Tushar Gangaram Imade";
+console.log("Full Name ->",myFullName);
+
+var myLove = "I Love Java";
+console.log("My Love ->", myLove);
+
+var firstName = "Tushar";
+var lastName = "Imade";
+var age = 30;
+var address = "Datta Nagar, Sangola";
+var pinCode = 413307;
+var gender = "Male";
+var isMarried = "Yes";
+
+console.log("First Name ->",firstName);
+console.log("Last Name ->",lastName);
+console.log("Age ->",age);
+console.log("Address ->",address);
+console.log("Pincode ->",pinCode);
+console.log("Gender ->",gender);
+console.log("IsMarried ->",isMarried);
+
+
+
+
+
